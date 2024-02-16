6ad6e149f24d0bddc74cdaeeef2d5e481dd4dba8
Dayo Olutayo <olutayo@olutayo.com>
2024-02-16 06:55:26 +0000

    Correct the import of setupApplicationTest helper

--- tests/integration/components/super-rentals-test.js
+++ tests/integration/components/super-rentals-test.js
@@ -1,9 +1,9 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'super-rental/tests/helpers';
 import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'super-rental/tests/helpers';
 
 module('Integration | Component | jumbo', function (hooks) {
-  setupRenderingTest(hooks);
+  setupApplicationTest(hooks);
 
   test('visiting /', async function (assert) {
     await visit('/');
